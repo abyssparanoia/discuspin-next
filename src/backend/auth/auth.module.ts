2a52c4ee066b7c567e8b09eeec7f858daa007d86
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-11 17:28:24 +0900

    feat: impl local strategy

--- src/backend/auth/auth.module.ts
+++ src/backend/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { PassportModule } from '@nestjs/passport'
+import { LocalStrategy } from './local.strategy'
 
 @Module({
-  providers: [AuthService]
+  imports: [PassportModule],
+  providers: [AuthService, LocalStrategy]
 })
 export class AuthModule {}
---
+++ src/backend/auth/local.strategy.ts
@@ -0,0 +1,16 @@
+import { Strategy } from 'passport-local'
+import { PassportStrategy } from '@nestjs/passport'
+import { Injectable } from '@nestjs/common'
+import { AuthService } from './auth.service'
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super()
+  }
+
+  async validate(token: string) {
+    const decodedToken = await this.authService.verifyIdToken(token)
+    return decodedToken
+  }
+}
