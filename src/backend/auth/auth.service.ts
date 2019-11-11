f435bfb5a7df74cba421cfb14ffb5321ab79d4ea
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-11 14:46:08 +0900

    feat: impl verufy token

--- src/backend/auth/auth.service.ts
+++ src/backend/auth/auth.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { auth } from '../../firebase/admin'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor() {}
+
+  async verifyIdToken(token: string) {
+    const decoedeToken = await auth.verifyIdToken(token).catch(err => {
+      throw new UnauthorizedException(err.message)
+    })
+
+    return decoedeToken
+  }
+}
