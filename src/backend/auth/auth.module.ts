978e5c0d3ba02543555f5d2a7bc820cbbb4fa804
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-17 17:45:46 +0900

    fix: remove unused module

--- src/backend/auth/auth.module.ts
+++ src/backend/auth/auth.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
-import { PassportModule } from '@nestjs/passport'
 import { AuthGuard } from './auth.guard'
 import { AuthController } from './auth.controller'
 import { UserService } from '../user/user.service'
 
 @Module({
-  imports: [PassportModule],
   providers: [AuthService, UserService, AuthGuard],
   controllers: [AuthController]
 })
